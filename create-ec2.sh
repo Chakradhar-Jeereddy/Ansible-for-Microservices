dafa22e27160bc1da75ac89acf15697425ed8f94
Chakradhar Jeereddy <79322615+Chakradhar-Jeereddy@users.noreply.github.com>
2022-03-28 00:47:20 -0400

    Update create-ec2.sh

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-source common.sh
+#source common.sh
 Zone_ID="Z0044635YWL0HFQYR5YV"
+LOG_FILE=/tmp/create-ec2.log
 
 COMPONENT=$1
 if [ -z "$1" ]; then
@@ -9,18 +10,18 @@ if [ -z "$1" ]; then
   exit
 fi
 
-Print "GET Image ID"
+echo "GET Image ID"
 AMI_ID=$(aws ec2 describe-images --filters \
 "Name=name, Values=Centos-7-DevOps-Practice" | jq .Images[].ImageId | sed -e 's/"//g') &>>${LOG_FILE}
 StatCheck $?
 
-Print "Get security group"
+echo "Get security group"
 SG_ID=$(aws ec2 describe-security-groups --filters \
 "Name=group-name, Values=robo-allow" | jq .SecurityGroups[].GroupId | sed -s 's/"//g') &>>${LOG_FILE}
 StatCheck $?
 
 create_ec2() {
-  Print "Create spot instance"
+  echo "Create spot instance"
   PRIVATE_IP=$(aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t2.micro \
@@ -28,7 +29,7 @@ create_ec2() {
     --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
     --security-group-ids $SG_ID | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
   StatCheck $?
-  Print "Create DNS record"
+  echo "Create DNS record"
   sed -e "s/component/${COMPONENT}/" -e "s/PRIVATE_IP/${PRIVATE_IP}/" dnsrecord.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${Zone_ID} --change-batch file:///tmp/record.json | jq &>>${LOG_FILE}
   StatCheck $?
